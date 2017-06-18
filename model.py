42c4ac646bb78ecae05dab732eaf000a029cb890
Xutai Ma <xutai_ma@jhu.edu>
2017-06-18 01:04:50 -0400

    add pack and pad for LSTM

--- model.py
+++ model.py
@@ -1,9 +1,13 @@
 import torch.nn as nn
 from torch.autograd import Variable
-
+import torch.nn.utils.rnn.pack_padded_sequence as pack
+import torch.nn.utils.rnn.pad_packed_sequence as pad
 class LanguageModel(nn.Module):
     def __init__(self, voc_size, dim_word, dim_rnn, num_layers, dropout_rate = 0.2):
         super(LanguageModel, self).__init__()
+        self.rnn_layers = num_layers
+        self.dim_rnn = dim_rnn
+
         self.dropout = nn.Dropout(dropout_rate)
         self.word_lut = nn.Embedding(voc_size, dim_word)
         self.lstm = nn.LSTM(dim_word, dim_rnn, num_layers, dropout=dropout_rate)
@@ -15,12 +19,17 @@ class LanguageModel(nn.Module):
         self.word_lut.weight.data.normal_(mean=0, std=std)
         self.lstm.weight.data.normal_(mean=0, std=std)
         self.output.data.normal_(mean=0, std=std)
-`
-    def forward(self, inputs, lengths, hidden):
+
+    def forward(self, inputs, lengths):
+        inputs = pack(inputs, lengths)
         emb = self.dropout(self.word_lut(inputs))
-        encode, hidden = self.lstm(emb, lengths)
+        hidden = self.init_hidden(inputs.data.size(1))
+        encode, hidden = self.lstm(emb, hidden)
+        encode = pad(encode)[0]
         encode = self.dropout(encode)
         output = self.output(encode.view(encode.size(0) * encode.size(1), encode.size(2)))
         return output.view(encode.size(0), encode.size(1), encode.size(2))
 
+    def init_hidden(self, batch_size):
+        return Variable(torch.zeros(self.num_layers, batch_size, self.dim_rnn))
 
--- train.py
+++ train.py
@@ -1,1 +1,41 @@
 import torch 
+import data
+from model import LanguageModel
+
+def validate
+def train(
+        train_data='data/penn/train.txt',
+        val_data='data/penn/validate.txt',
+        batch_size=64,
+        val_batch_size=64,
+        model_name='LSTM_LM',
+        reload_=True,
+        optimizer='sgd',
+        lr=0.1,
+        epoch=20,
+        preprocess='reload',
+        cuda=True,
+        dim_word=256,
+        dim_rnn=512,
+        num_layers=1,
+        dropout_rate=0.3
+        ):
+
+    train_dataset = data.DataSet(train_dataset, batch_size)
+    val_dataset = data.DataSet(val_data, val_batch_size)
+
+    model = LanguageMode(
+            train_dataset.num_vocb,
+            dim_word = dim_word,
+            dim_rnn = dim_rnn,
+            num_layers = num_layers,
+            dropout_rate = dropout_rate
+            )
+    for epoch_idx in range(epoch):
+        batch_data, batch_lengths = train_dataset[epoch_idx]
+        output = model.forward(batch_data;)
+
+
+if __name__ == '__main__':
+    
+    train(arg)
