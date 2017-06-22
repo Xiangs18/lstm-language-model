c72eeadb698d92c74a1ca975af2c7fd97a902ea8
Xutai Ma <xutai_ma@jhu.edu>
2017-06-21 22:38:06 -0400

    ptb example

--- example/train_ptb.sh
+++ example/train_ptb.sh
@@ -2,22 +2,24 @@
 source /export/b18/xma/virtual/PyTorch/bin/activate
 GPU=`/home/pkoehn/statmt/bin/free-gpu`
 LM_PATH='/export/b18/xma/machine_translation/lstm-language-model/'
-#echo Using GPU $GPU
-CUDA_VISIBLE_DEVICES=$GPU python -u $LM_PATH/main.py \
+echo Using GPU $GPU
+CUDA_VISIBLE_DEVICES=$GPU python -u $LM_PATH/train.py \
   --train_data './data/penn/train.txt.prep.train.pt' \
   --val_data './data/penn/valid.txt.prep.val.pt' \
-  --model_name 'model/penn-lm' \
-  --dim_word 256 \
-  --dim_rnn  256\
+  --model_name 'model/exp8-lstm-lm' \
+  --dim_word 200 \
+  --dim_rnn  200\
   --num_layers 2 \
   --batch_size 64 \
   --val_batch_size 64 \
   --epoch 10 \
-  --optimizer sgd \
+  --optimizer SGD \
   --lr 1 \
   --lr_decay 0.9 \
-  --dropout_rate 0.5 \
-  --display_freq 20\
+  --dropout_rate 0.3 \
+  --display_freq 100 \
   --save_freq 0\
   --cuda \
   --clip 5 \
+
+
